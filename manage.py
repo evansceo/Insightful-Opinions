c68e00cd73c33399760cca99933f22b286a01c05
evansceo <evans.onyiego@student.moringaschool.com>
2021-11-15 09:18:44 +0300

    manage.py mordified

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@ from flask_script import Manager,Server
 from app.models import User
 from  flask_migrate import Migrate, MigrateCommand
 
+#Creating app instance
+app = create_app('development')
+# app = create_app('test')
+
 
 
 

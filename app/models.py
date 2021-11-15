032f1462b95a0ec790f4d6b7e91ce5a90ef86ecd
evansceo <evans.onyiego@student.moringaschool.com>
2021-11-15 10:09:54 +0300

    model.py file mordified

--- app/models.py
+++ app/models.py
@@ -74,4 +74,8 @@ class Comment(db.Model):
     def save_comment(self):
         db.session.add(self)
         db.session.commit()
+    @classmethod
+    def get_comments(cls, blog_id):
+        comments = Comment.query.filter_by(blog_id=blog_id).all()
+        return comments
    

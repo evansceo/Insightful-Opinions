ba8434213193db3661999c5157b1d2cd87ba2329
evansceo <evans.onyiego@student.moringaschool.com>
2021-11-15 09:41:24 +0300

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,12 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 
+def create_app(config_name):
+    
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
 
     return app
